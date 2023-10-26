69ed7aaa6157e1cd635b889d70b8c951835ae969
Nuradi <nuradi@gmail.com>
2023-10-26 14:48:28 +0300

    finish 2 lesson HW

--- js.js
+++ js.js
@@ -1,13 +1,131 @@
-//Первый урок
+//Второе задание
+//1 задание
+// let a = +prompt("Первое число");
+// let b = +prompt("Второе число");
+// let result = a + b;
 
-let str = "lorem";//string
-let num = 123 //number
-let bo = true; // boolean
-//Если использовать typeof покажет какой тип данных
-console.log(str,num,bo);
+// console.log(result);
 
-let name = prompt('Ваше имя');
-let adress = prompt("Ваша Почта");
-let phone = +prompt('Ваш номер телефона');
+// 2 Задание
+// let x = +prompt("Первое число");
+// let y = +prompt("Второк число");
 
-console.log(`Студент: ${name}. Адрес: ${adress}. Номер: ${phone}`);
+// let result2 = x*y;
+
+// console.log(result2);
+
+// 3 задание
+// let a = +prompt("Напишите число чтобы узнать число положительное или отрицательное");
+
+// if (a>0) {
+//     console.log("Положительное Число");
+// }else{
+//     console.log("Отрицательное число");
+// }
+
+
+// 4 Задание 
+// let a = 9;
+// let b = 7;
+// let result = a*b;
+
+// console.log(result);
+
+
+//5 Задание
+// let x =45; 
+// let y = 4;
+// if (x>y) {
+//     console.log(true);
+// }else if(x==y){
+//     console.log("Одинаковые");
+// }else{console.log(false);}
+
+//6 Задание
+// let age = +prompt("Ваш возраст");
+
+// if (age<=10) {
+//     console.log("Ребенок");
+// }else if(age<=20){
+// console.log("Подросток");
+// }else if(age<40){
+//     console.log("Взрослый");
+// }else if(age>=55){
+//     console.log("Пожилой");
+// }
+
+//8
+// function f(a,b) {
+//     return a + b;
+// }
+// console.log(f(25,25));
+
+//9 
+// let fruits = ['banan',"apple",'orange'];
+
+// for (let i = 0; i < fruits.length; i++) {
+//         console.log(fruits[i]);
+// }
+
+//9 а как я понял)
+// let counter = 0;
+
+// counter +=5;
+// console.log(counter);
+
+//10 
+
+// let text = 'lorem ipsum';
+
+//11
+// let textPro = prompt('Введите текст чтобы сравнить');
+
+
+// if (text===textPro) {
+//     console.log("Welcome");
+// }else{
+//     console.log("Ошибка в тексте");
+// }
+
+// alert('lorem ipsum ;)');
+
+//12 Не помню как сделать с помощю for поэтому сделал так
+
+// function f(...rest) {
+
+//   let result = rest.reduce((sum,item) => {
+//        return sum+item;
+//     },0);
+//     return result
+// }
+
+// console.log(f(25,25));
+
+//13 //Если возраст age будет Больше 18 то будет true иначе false
+// let  age = 20;
+
+// let isAdult = (age>18) ? true : false;
+// console.log(isAdult);
+
+
+//14
+// let ages = +prompt("Возраст?");
+
+// let test = (ages>=18) ? console.log('Ваш возраст подходит') : console.log('Ваш возраст не подходит к сожалению(');
+
+//Функции
+//1
+// function f(a,b) {
+//     return console.log(a+b);
+// }
+// f(15,85)
+
+//2 
+// function x(text) {
+//     return console.log(text.length);
+// }
+
+// x('Nksdfikjsdfg');
+
+//3 не знаю как добавить опретор в параметр функции
+//4 Тоже не знаю 
